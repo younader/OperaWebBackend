60f2a7fae4c9c148fa3902e3cdccd1a53e0ab8ee
omar abdelalim <omarabdelalim@yahoo.com>
2019-12-25 01:18:36 +0200

    edit event added

--- functions/event/controller.js
+++ functions/event/controller.js
@@ -24,6 +24,35 @@ exports.addEvent = async (req, res) => {
 
 }
 
+exports.editEvent = async (req, res) => {
+
+    const eventName=await Event.findAll({
+        where:{
+            name:req.body.name
+        }
+    });
+    const hall = await Hall.findAll({
+        where:{
+            id:req.body.hallId
+        }
+    });
+    if(eventName.length!=0){return res.send({message:'event name already registered'})};
+    if(hall.length==0){return res.send({message:'hall ID not registered'})};
+
+    const event = await Event.findAll({
+        where:{
+            id:req.params.eventId
+        }
+    });
+    if(event.length==0) return res.send({message:'event doesn\'t exist'})
+    
+    event = req.body;
+    const result = await event.save();
+    
+    return res.send({message:'event registered successfully'})
+
+}
+
 exports.getEmptySeats = async (req,res) => {
     const reses = await Reservation.findAll({
         where:{
